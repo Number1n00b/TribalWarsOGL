4422cb08989e28f69d2f09ef84db027e9dc5b296
Chris <cebrunette@gmail.com>
2017-08-10 21:42:16 +0800

    Initial port.

---
+++ OpenGLFromScratch/src/new_main.cpp
@@ -0,0 +1,34 @@
+#pragma comment(lib, "winmm.lib")
+#pragma comment(lib, "opengl32.lib")
+#pragma comment(lib, "glu32.lib")
+#pragma comment(lib, "glew32.lib")
+#pragma comment(lib, "SDL2.lib")
+#pragma comment(lib, "SDL2main.lib")
+
+//Standard libs.
+#include <iostream>
+#include <windows.h>
+
+//Graphics libs.
+// #include <GL/glew.h>
+
+//Libraries for mouse pointer manipulation and other IO.
+// #include <SDL.h>
+// #include <SDL_syswm.h>
+
+//Own header file.
+#include "main.h"
+
+//glm header.
+#include <glm/glm.hpp>
+
+//Using / Namespace declarations.
+using std::cin;
+using std::cout;
+using std::endl;
+using glm::vec3;
+
+
+int main(void){
+   cout << "Test" << endl;
+}
