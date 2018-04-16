ec5802516e44d98f43e36884deb2de1d6fb5a2ee
Number1n00b <cebrunette@gmail.com>
2018-04-16 11:48:53 +0800

    started Linked List work

---
+++ src/tests/LinkedListTest.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../util/LinkedList.h"
+
+class te{
+    public:
+        te(){
+            //Empty
+        }
+
+        int x;
+};
+
+int main(void){
+    printf("============ Testing LinkedList ============\n");
+
+    LinkedList<te>* list = new LinkedList<te>;
+
+
+
+
+    return 0;
+}
--- src/util/LinkedList.h
+++ src/util/LinkedList.h
@@ -1,21 +1,39 @@
 #ifndef LINKED_LIST_H
 #define LINKED_LIST_H
 
+template <class T>
+struct LinkedListNode{
+	LinkedListNode<T>* next;
+	LinkedListNode<T>* prev;
+
+	T* data;
+};
+
+
 template<class T>
 class LinkedList
 {
 	public:
-        LinkedList<T>();
+        LinkedList();
 
-        T RemoveElement(index ii);
-        T GetElement(index ii);
+		void AddElementToStart(const T& element);
+		void AddElementToEnd(const T& element);
 
-        void AddElement(index ii);
+        T* GetElement(int index);
+		void DeleteElement(int index);
+		T* RemoveElement(int index);
 
+		int Length();
+
+		~LinkedList();
 	private:
-        //@TODO later we can make this of type <Entities>
-        //and also have one for type <drawables>
-        LinkedList<Entity>* entities;
+		LinkedListNode<T>* m_Head;
+		LinkedListNode<T>* m_Tail;
+
+		LinkedListNode<T>* GetNode(int index);
+
+		int m_NodeCount;
 };
+
 
 #endif //LINKED_LIST_H
